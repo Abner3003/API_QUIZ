12f01ded9692eb85c5c27221a7af995b3035ea1d
abner.albino <abner.albino@BRWGWQTVG2>
2020-01-21 17:33:56 -0300

    initial

---
+++ Quiz.Dominio.API/DataBase/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz.Dominio.API.DataBase.Models
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public int CD_PESSOA { get; set; }
+        public string DS_NOME { get; set; }
+        public string DS_EMAIL { get; set; }
+        public string DS_SENHA { get; set; }
+       }
+}
