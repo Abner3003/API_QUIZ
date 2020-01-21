51d0333d2bce7465b5c899b6842b9ffb18599839
abner.albino <abner.albino@BRWGWQTVG2>
2020-01-21 17:59:32 -0300

    initial commit

---
+++ API_QUIZ/Quiz.Dominio.API/DataBase/Contexto/QuizContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Quiz.Dominio.API.DataBase.Contexto
+{
+    public class QuizContext : DbContext
+    {
+        public QuizContext(DbContextOptions<QuizContext> options) : base(options)
+        {
+
+        }
+    }
+}
